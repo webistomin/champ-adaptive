f83421b64d13196dab20999f09b7f15cc082b4fb
webistomin <webistomin@gmail.com>
2019-03-17 19:03:43 +0300

    Update 2019-03-17T16:03:15.699Z

--- gulp/tasks/clean.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-const del = require('del');
-
-gulp.task('clean:build', function() {
-  return del('./build');
-});
--- gulp/tasks/test/jslint.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-
-gulp.task('js:lint', () => (
-  gulp.src(['src/js/**/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-));
